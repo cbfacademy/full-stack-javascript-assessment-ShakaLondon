98550d6da9fb2e93dc34b9a21cb3b3caf9e67043
Shakira Pingue <sm880@kent.ac.uk>
2023-12-27 00:05:00 +0000

    Update Container and Button components and add css

--- packages/frontend/src/components/utils/containers.jsx
+++ packages/frontend/src/components/utils/containers.jsx
@@ -1,6 +1,9 @@
-export const Container = ({ children, nameID = null, fullWidth = false, flex, classes = '' }) => {
+export const Container = ({ children, nameID = null, size = 'size', height = null, width = null, flex, classes = '', align = 'center', justify = 'center', text = 'center' }) => {
     return (
-        <div id={ nameID } className={`container${ fullWidth && '-full-size' } ${ flex && 'display-flex'} ${ classes + 'align-center justify-center text-center' }`}>
+        <div 
+            id={ nameID } 
+            className={`container${ size && `-full-${size}` } ${ flex && 'display-flex'} align-${ align } justify-${ justify } text-${ text } ${classes}`}
+            style={{ width: width, height: height }}>
             { children }
         </div>
     )
