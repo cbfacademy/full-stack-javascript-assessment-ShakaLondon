69c40f3928ae74274177e56d5fd4ca2c8c183204
Shakira Pingue <sm880@kent.ac.uk>
2023-12-27 00:13:41 +0000

    Update error-page

--- packages/frontend/src/views/error-page/error-page.jsx
+++ packages/frontend/src/views/error-page/error-page.jsx
@@ -1,6 +1,20 @@
+import { PrimaryButton } from '../../components/utils/buttons';
+import { Container } from '../../components/utils/containers';
 import { ViewPage } from '../../components/utils/pages'
 
 const ErrorPage = () => {
+    return (
+        <ViewPage nameID='error-page' flex >
+          <Container flex classes='flex-column'>
+            <h>Oops!</h>
+            <p5 className='py-half'>Sorry, an unexpected error has occurred.</p5>
+            <p>
+              {/* <i>{error.statusText || error.message}</i> */}
+            </p>
+            <PrimaryButton classes='bg-grey-one white'>Go Home</PrimaryButton>
+          </Container>
+        </ViewPage>
+      );
   };
   
 export default ErrorPage;
