19d090f41f363d55444df1761dd71fc38c64981e
Shakira Pingue <sm880@kent.ac.uk>
2023-12-27 00:57:40 +0000

    Create profile-page

---
+++ packages/frontend/src/views/profile-page/profile-page.jsx
@@ -0,0 +1,12 @@
+import { PrimaryButton } from '../../components/utils/buttons';
+import { Container } from '../../components/utils/containers';
+import { ViewPage } from '../../components/utils/pages'
+
+const ProfilePage = () => {
+    return (
+        <ViewPage nameID='profile-page' flex >
+        </ViewPage>
+      );
+  };
+  
+export default ProfilePage;
