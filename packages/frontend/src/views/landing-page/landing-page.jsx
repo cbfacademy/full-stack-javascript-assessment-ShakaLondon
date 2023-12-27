6c9f076d4d8de3892ac4d3f86169348ac9838cdf
Shakira Pingue <sm880@kent.ac.uk>
2023-12-27 00:58:11 +0000

    Update landing-page

--- packages/frontend/src/views/landing-page/landing-page.jsx
+++ packages/frontend/src/views/landing-page/landing-page.jsx
@@ -1,4 +1,14 @@
+import { Container } from "../../components/utils/containers";
+import { ViewPage } from "../../components/utils/pages";
+
 const LandingPage = () => {
+    return (
+        <ViewPage nameID='home-page' flex >
+            <Container flex>
+                hello
+            </Container>
+      </ViewPage>
+    )
 };
   
 export default LandingPage;
