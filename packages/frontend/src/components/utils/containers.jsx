39b08cc0dc95d6189fec342731bdb94ba9577a60
Shakira Pingue <sm880@kent.ac.uk>
2023-12-30 04:00:42 +0000

    Update how classes are added to reusable components

--- packages/frontend/src/components/utils/containers.jsx
+++ packages/frontend/src/components/utils/containers.jsx
@@ -1,10 +1,21 @@
-export const Container = ({ children, nameID = null, size = 'size', height = null, width = null, flex, classes = '', align = 'center', justify = 'center', text = 'center' }) => {
+export const Container = ({ children, nameID=null, size=null, height=null, width=null, flex, classes='', align='center', justify='center', text='center' }) => {
     return (
         <div 
             id={ nameID } 
-            className={`container${ size && `-full-${size}` } ${ flex && 'display-flex'} align-${ align } justify-${ justify } text-${ text } ${classes}`}
+            className={`container${ size ? `-full-${size}` : ''} ${ flex ? 'display-flex' : ''} align-${ align } justify-${ justify } text-${ text } ${classes}`}
             style={{ width: width, height: height }}>
             { children }
         </div>
-    )
+    );
+};
+
+export const Box = ({ children, nameID=null, height=null, width=null, flex, classes='', align='center', justify='center', text='center' }) => {
+    return (
+        <div 
+            id={ nameID } 
+            className={`${ flex ? 'display-inline-flex' : ''} align-${ align } justify-${ justify } text-${ text } ${classes}`}
+            style={{ width: width, height: height }}>
+            { children }
+        </div>
+    );
 };
--- packages/frontend/src/views/error-page/error-page.jsx
+++ packages/frontend/src/views/error-page/error-page.jsx
@@ -5,13 +5,13 @@ import { ViewPage } from '../../components/utils/pages'
 const ErrorPage = () => {
     return (
         <ViewPage nameID='error-page' flex >
-          <Container flex classes='flex-column'>
+          <Container flex size='size' classes='flex-column'>
             <h>Oops!</h>
             <p5 className='py-half'>Sorry, an unexpected error has occurred.</p5>
             <p>
               {/* <i>{error.statusText || error.message}</i> */}
             </p>
-            <PrimaryButton classes='bg-grey-one white'>Go Home</PrimaryButton>
+            <PrimaryButton classes='bg-grey-one white' link='home' >Go Home</PrimaryButton>
           </Container>
         </ViewPage>
       );
