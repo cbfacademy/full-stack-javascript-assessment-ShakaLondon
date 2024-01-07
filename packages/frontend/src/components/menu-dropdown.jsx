2017bc8b2bd2a43ff08205562dfffdf9c10732e5
Shakira Pingue <sm880@kent.ac.uk>
2024-01-07 02:19:00 +0000

    Add menu dropdown

--- packages/frontend/src/components/menu-dropdown.jsx
+++ packages/frontend/src/components/menu-dropdown.jsx
@@ -1,12 +1,5 @@
 import { Container, Box } from "./utils/containers";
-import { ReactComponent as CircleArrowLeft } from '../assets/svg/circle-arrow-left.svg'
-import { ReactComponent as MenuBars } from '../assets/svg/menu-bars.svg'
-import { ReactComponent as ProfileCircle } from '../assets/svg/profile-circle.svg'
-import { ReactComponent as CrossExit } from '../assets/svg/cross-exit.svg'
-import { getProfileState } from "../redux/slices/app-state-slice";
 import { useSelector } from "react-redux";
-import { navigateTo } from "../hooks/location-path-hooks";
-import { SVGLink } from "./utils/svg-link";
 
 export const MenuDrop = () => {
 
@@ -15,18 +8,8 @@ export const MenuDrop = () => {
     return (
         <>
             { menuOpenState &&
-                <Container flex size='height' width='33vw' justify='between' align='start' classes='fixed bg-grey-one right'>
-                    <Box flex>
-                        {/* <SVGLink link='home' >
-                            { profileOpenState && <CircleArrowLeft className='svg-icon' onClick={ () => navigateTo('home')} />}
-                        </SVGLink>
-                    </Box>
-                    <Box flex>
-                        <SVGLink link='profile' >
-                            { !profileOpenState  && <ProfileCircle className='svg-icon' onClick={ () => navigateTo('profile')} />}
-                        </SVGLink>
-                        { !profileOpenState  && <MenuBars className='svg-icon' onClick={ () => navigateTo('')} />}
-                        { !profileOpenState  && <CrossExit className='svg-icon' onClick={ () => navigateTo('')} />} */}
+                <Container id='menu-dropdown' flex size='height' width='33vw' justify='between' align='start' classes='fixed bg-grey-one right'>
+                    <Box flex justify='center' align='center'>
                     </Box>
                 </Container>
             }
