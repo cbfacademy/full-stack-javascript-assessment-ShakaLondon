564effc8f86d6973f7d8254c2e92f355b98ba383
Shakira Pingue <sm880@kent.ac.uk>
2024-01-21 22:57:20 +0000

    Setup server and add error handlers

--- packages/backend/index.js
+++ packages/backend/index.js
@@ -1,15 +1,23 @@
-const express = require("express");
-const helmet = require("helmet");
-const cors = require("cors");
-const { MongoClient, ServerApiVersion } = require("mongodb");
+import express from "express";
+import helmet from "helmet";
+import cors from "cors";
+import { MongoClient, ServerApiVersion } from "mongodb";
+import listEndpoints from "express-list-endpoints";
+import mongoose from "mongoose";
 
-require("dotenv").config();
+import dotenv from 'dotenv'
+import { catchAllErrorHandler, entryForbiddenMiddleware, notFoundMiddleware } from "./services/utils/errorhandler.js";
+dotenv.config()
+
+
+// SETUP APP
 const app = express();
 
 app.use(helmet());
 app.use(cors());
 app.use(express.json());
 
+// CONNECT TO MONGO
 const uri = process.env.MONGO_URI; // Add your connection string from Atlas to your .env file. See https://docs.atlas.mongodb.com/getting-started/
 const client = new MongoClient(uri, {
   serverApi: {
@@ -28,11 +36,22 @@ client.connect((err) => {
   client.close();
 });
 
-app.get("/", (req, res) => {
-  res.send("Hello from the CBF Academy backend!");
-});
 
+// ERROR HANDLERS
+app.use(notFoundMiddleware);
+app.use(entryForbiddenMiddleware);
+app.use(catchAllErrorHandler);
+
+// CREATE APP TABLE
+console.table(listEndpoints(app));
+
+// LISTEN TO APP
 const PORT = process.env.PORT || 5000;
-app.listen(PORT, () => {
-  console.log(`Server started on http://localhost:${PORT}`);
+app.listen(PORT, async () => {
+  try {
+    await mongoose.connect(process.env.MONGO_URI);
+    console.log(`✅ Server is running on ${PORT} and connected to MongoDB`);
+  } catch (error) {
+    console.log("Db connection is failed ", error);
+  }
 });
