749bab2b86e2c35b52647ed465a128fb402f12d1
Shakira Pingue <sm880@kent.ac.uk>
2024-01-28 22:27:41 +0000

    Connect profile form to redux state

--- packages/frontend/src/views/profile-page/profile-page.jsx
+++ packages/frontend/src/views/profile-page/profile-page.jsx
@@ -5,16 +5,42 @@ import { ProfileFormOne } from '../../components/profile-page/profile-form-one'
 import { useDispatch, useSelector } from 'react-redux';
 import { navigateTo } from '../../hooks/location-path-hooks';
 import { profileOpen } from '../../redux/slices/app-state-slice';
+import { userLogin } from '../../redux/slices/user-state-slice';
+import { useEffect, useState } from 'react';
+import userData from '../../services/user/user-data';
 
 const ProfilePage = () => {
 
-  const loggedInState = useSelector((state) => state.userState.loggedIn)
   const appDispatch = useDispatch()
+  const loggedInState = useSelector((state) => state.userState.loggedIn)
+  const userState = useSelector((state) => state.userState.user)
+
+  const [getUser, setUser] = useState({
+    name: userState.name,
+    surname: userState.surname,
+    username: userState.username,
+    dateOfBirth: userState.dateOfBirth,
+      email: userState.email,
+      password: userState.password,
+      childsName: userState.childsName,
+      childDateOfBirth: userState.childDateOfBirth,
+    });
 
   if (!loggedInState) { 
     appDispatch(profileOpen(false))
     return navigateTo('login')
   }
+
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    userData.editUser(userState._id, getUser)
+};
+
+const handleChange = (event) => {
+    const { name, value } = event.target;
+    setUser({ ...getUser, [`${name}`]: value });
+};
 
     return (
         <ViewPage nameID='profile-page' flex classes=''>
@@ -23,7 +49,7 @@ const ProfilePage = () => {
           </PageContainer>
           <PageContainer nameID='profile-page-form-cont' flex height='60vh' justify='center' align='center'>
             <Column nameID='profile-page-header-col-1' flex small={8} med={8} large={8} align='baseline' justify='start'>
-              <ProfileFormOne/>
+              <ProfileFormOne handleChange={ handleChange } getUser={ getUser }/>
             </Column>
             <Column nameID='profile-page-header-col-2' classes='' small={8} med={8} large={8} justify='center' align='center'>
             </Column>
