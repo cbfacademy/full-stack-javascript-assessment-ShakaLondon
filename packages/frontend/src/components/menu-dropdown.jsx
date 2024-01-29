7ca2cac47a47f8d38d686627cc9c243fe2a73bb8
Shakira Pingue <sm880@kent.ac.uk>
2024-01-29 15:18:19 +0000

    Add login, register and logout buttons to menu dropdown

--- packages/frontend/src/components/menu-dropdown.jsx
+++ packages/frontend/src/components/menu-dropdown.jsx
@@ -1,16 +1,36 @@
-import { Container, Box } from "./utils/containers";
-import { useSelector } from "react-redux";
+import { menuOpen } from "../redux/slices/app-state-slice";
+import { logout } from "../redux/slices/user-state-slice";
+import { PrimaryButton } from "./utils/buttons";
+import { Container } from "./utils/containers";
+import { useDispatch, useSelector } from "react-redux";
 
 export const MenuDrop = () => {
 
     const menuOpenState = useSelector((state) => state.appState.menuOpen)
+    const loggedInState = useSelector((state) => state.userState.loggedIn)
+    const appDispatch = useDispatch()
+
+    const menuHandleClick = (dispatchFunction) => {
+        if (dispatchFunction) {
+        appDispatch(dispatchFunction);
+        };
+        appDispatch(menuOpen(false));
+    };
 
     return (
         <>
             { menuOpenState &&
-                <Container nameID='menu-dropdown-cont' flex size='height' width='33vw' justify='between' align='start' classes='fixed bg-grey-one right'>
-                    <Box nameID='menu-dropdown-box' flex justify='center' align='center'>
-                    </Box>
+                <Container nameID='menu-dropdown-cont' flex size='height' width='33vw' justify='center' align='content-end' classes='fixed bg-grey-one right pb-5'>
+                    <Container nameID='menu-dropdown-box' justify='center' align='center' >
+                    <PrimaryButton nameID='menu-drop-login-btn' link={ !loggedInState ? 'login' : 'home' } width='60%' classes='mt-1 button-invert' click={ () => !loggedInState ? menuHandleClick() : menuHandleClick( logout() ) } >
+                    { !loggedInState ? 'Login' : 'Logout' }
+                </PrimaryButton>
+                </Container>
+                <Container nameID='menu-dropdown-box' justify='center' align='center' >
+                { !loggedInState && <PrimaryButton nameID='menu-drop-register-btn' link='register' width='60%' classes='mt-1 button-invert' click={ () => menuHandleClick() } >
+                    Register
+                </PrimaryButton>}
+                </Container>
                 </Container>
             }
         </>
