020b613d1d5b34f7c03f9ba384ac83ea3f44f18a
Shakira Pingue <sm880@kent.ac.uk>
2024-01-29 15:01:27 +0000

    Connect profile header values to redux

--- packages/frontend/src/components/profile-page/profile-header.jsx
+++ packages/frontend/src/components/profile-page/profile-header.jsx
@@ -2,19 +2,19 @@ import { Box, Column, Container } from "../utils/containers";
 import { Image } from "../utils/image";
 import { Text } from "../utils/text";
 
-export const ProfileHeader = () => {
+export const ProfileHeader = ({ getUser }) => {
     return (
         <Container nameID='profile-header' flex size='size' justify='center' align='center'>
 
-            <Column nameID='profile-header-col-1' flex small={11} med={11} large={11} justify='end' align='start' classes='ps-3'>
+            <Column nameID='profile-header-col-1' flex small={16} med={11} large={11} justify='end' align='start' classes='ps-3'>
                 <Box flex height='250px' classes='flex-column' align='start' justify='center'>
-                    <Text size='h3'>Child's Name</Text>
+                    <Text size='h3'>{getUser?.childsName}</Text>
                     <Text size='p5'>Child's Name</Text>
                 </Box>
             </Column>
-            <Column nameID='profile-header-col-2' flex small={5} med={5} large={5} justify='end' align='end' classes='pe-3'>
+            <Column nameID='profile-header-col-2' flex small={16} med={5} large={5} justify='end' align='end' classes='pe-3'>
                 <Box flex height='250px' justify='center' align='center'>
-                    <Image src='https://placehold.co/200x200' imgClasses='rounded' align='self-center' justify='center'/>
+                    <Image src={getUser?.avatar} imgClasses='rounded profile-header-avatar' align='self-center' justify='center'/>
                 </Box>
             </Column>
         </Container>
