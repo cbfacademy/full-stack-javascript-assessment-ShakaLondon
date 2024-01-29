253a75c8884d48f466a27e5252aaf55217c1549c
Shakira Pingue <sm880@kent.ac.uk>
2024-01-29 15:05:19 +0000

    Update correct file type

--- packages/frontend/src/components/user-page/login-page.jsx
+++ packages/frontend/src/components/user-page/login-page.jsx
@@ -1,5 +1,5 @@
 import { useState } from 'react';
-import { PageContainer } from '../utils/containers';
+import { Box, PageContainer } from '../utils/containers';
 import { FormGroup, FormFloatingInput, FormInput } from '../utils/form';
 import { PrimaryButton } from '../utils/buttons';
 
@@ -7,14 +7,16 @@ const LoginPage = ({ handleChange, getUser, handleSubmit }) => {
 
     return (
           <PageContainer nameID='login-page-cont' flex size='size' classes='flex-column' justify='center' align='center'>
-            <FormGroup nameID='login-page-form' classes='flex-column' justify='center' align='center' width='50%' onSubmit={ handleSubmit }>
+            <FormGroup nameID='login-page-form' classes='flex-column register-form' justify='center' align='center' width='50%' onSubmit={ handleSubmit }>
                 <FormFloatingInput nameID='login-page-email' type='email' value={ getUser.email } inputName="email" placeholder='Enter your email here' label='Email' handleChange={ handleChange }/>
                 <FormFloatingInput nameID='login-page-password' type='password' value={ getUser.password } inputName="password" placeholder='Enter your password here' label='Password' handleChange={ handleChange }/>
-                <FormInput nameID='login-page-btn' type='submit' value='Login' size='p5'/>
+                <FormInput nameID='login-page-btn' type='submit' value='Login' size='p5' inputWidth='60%' classes='mt-1'/>
             </FormGroup>
-                <PrimaryButton nameID='login-page-register-btn' link='register' classes='mt-1'>
+            <Box nameID='login-page-register-box'>
+                <PrimaryButton nameID='login-page-register-btn' link='register' classes='mt-1' width='60%' >
                     Register
                 </PrimaryButton>
+                </Box>
           </PageContainer>
       );
   };
