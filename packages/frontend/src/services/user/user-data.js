ae20057a72c6c03f7d907f1fdcca950fe88b18c9
Shakira Pingue <sm880@kent.ac.uk>
2024-01-29 15:32:16 +0000

    Add update user and update image functions

--- packages/frontend/src/services/user/user-data.js
+++ packages/frontend/src/services/user/user-data.js
@@ -2,11 +2,17 @@ import authHeader from "../axios/header.js";
 import api from "../axios/api.js";
 
 const editUser = (userUpdate) => {
-  return api.put("users/login/update", userUpdate, { headers: authHeader() });
+  return api.put("users/update", userUpdate, { headers: authHeader() });
 };
 
 const editAvatar = (userUpdateImage) => {
-  return api.post("image/upload/avatar", userUpdateImage, {
+  return api.post("assets/upload/UserImage", userUpdateImage, {
+    headers: authHeader('multipart/form-data'),
+  });
+};
+
+const updatePassword = (existingPassword, newPassword) => {
+  return api.put("users/updatePassword", {existingPassword: existingPassword, newPassword: newPassword}, {
     headers: authHeader(),
   });
 };
@@ -14,6 +20,7 @@ const editAvatar = (userUpdateImage) => {
 const userData = {
   editUser,
   editAvatar,
+  updatePassword,
 };
 
 export default userData
