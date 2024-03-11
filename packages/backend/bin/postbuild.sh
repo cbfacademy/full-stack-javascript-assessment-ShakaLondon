cc63a992af5ef8c133e6adfbba522001f15dc475
Shakira Pingue <sm880@kent.ac.uk>
2024-03-11 23:55:30 +0000

    Add env mongo string

--- packages/backend/bin/postbuild.sh
+++ packages/backend/bin/postbuild.sh
@@ -12,3 +12,5 @@ cp -r ./node_modules ./.amplify-hosting/compute/default/node_modules
 cp -r public ./.amplify-hosting/static
 
 cp deploy-manifest.json ./.amplify-hosting/deploy-manifest.json
+
+export MONGO_URI=$(aws ssm get-parameter --name "/amplify/dxrmrwkfi1p5r/dynamo-learning-app-backend/MONGO_URI" --with-decryption --query "Parameter.Value" --output text)
